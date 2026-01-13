709dd20699d4784aa7c61d3603099ff185008420
Vikash-980 <vikashkumar843399@gmail.com>
2026-01-13 09:55:30 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {createServer} from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js"
+import { connect } from "node:http2";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://Vikash_kumar_07:Vikash678@cluster0.6is3myq.mongodb.net/?appName=Cluster0");
+    console.log("MongoDB connected");
+    server.listen(app.get("port"), () => {
+        console.log("Listening on port 8000")
+    });
+}
+
+start();
